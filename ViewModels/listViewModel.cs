0cb60552de85c1ceac534bbeb144847f971c8f1a
SuperSASS <32005046+SuperSASS@users.noreply.github.com>
2022-06-04 13:48:56 +0800

    Complete ListView……

--- ViewModels/listViewModel.cs
+++ ViewModels/listViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using CourseDesign.Command.Classes;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CourseDesign.Command.Classes.T_DollClass;
 
 namespace CourseDesign.ViewModels
 {
-    internal class ListViewModel
+    internal class ListViewModel : BindableBase
     {
+        private ObservableCollection<T_DollClass> t_dolls;
+
+        public ObservableCollection<T_DollClass> T_Dolls
+        {
+            get { return t_dolls; }
+            set { t_dolls = value; }
+        }
+
+        public ListViewModel()
+        {
+            T_Dolls = new ObservableCollection<T_DollClass>();
+            TEST_CreateTDolls();
+        }
+
+        void TEST_CreateTDolls()
+        {
+            for (int i = 1; i <= 15; i++)
+                T_Dolls.Add(new T_DollClass(i, "SuperSASS", i % 4 + 1, (typeEnum)(i % 6), @"/Assets/T-Dolls/T-Doll-1.png"));
+        }
     }
 }
--- Views/TaskView.xaml.cs
+++ Views/TaskView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CourseDesign.Command.Classes;
+using CourseDesign.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,52 +23,20 @@ namespace CourseDesign.Views
     /// </summary>
     public partial class TaskView : UserControl
     {
+
         public TaskView()
         {
             InitializeComponent();
-            AddHandler(ListBox.MouseWheelEvent, new MouseWheelEventHandler(ListBox_HorizontalMouseWheel), true);
-        }
-
-        /// <summary>
-        /// 将ScrollViewer（即该页面中的水平滚动模块task_list）的鼠标滚动事件，响应为可以水平滚动。
-        /// <para>参考自：http://www.manongjc.com/detail/22-ggnbscnaxjwpqhx.html 和 https://blog.csdn.net/weixin_30680385/article/details/97466338</para>
-        /// </summary>
-        /// <param name="sender">这个页面类</param>
-        /// <param name="e">鼠标滚轮移动事件</param>
-        private void ListBox_HorizontalMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            // sender代表的就是窗口，则sender.AcutalWidth与滚轮的width呈线性关系，或许可以推算滚动相对距离
-            // TaskView items = (TaskView)sender;
-            ScrollViewer scroll = task_list;
-            if (scroll != null)
-            {
-                int d = e.Delta;
-                // int currentNum = 0;
-                // var horOffset = (int)Math.Round(scroll.HorizontalOffset);
-                if (d > 0)
-                {
-                    scroll.LineLeft();
-                    scroll.LineLeft();
-                    // currentNum = horOffset / (int)items.ActualWidth + 1; // 不知道原作者为什么会算这个完全没用到，估计就是想用这个来确定相对滚动距离然后滚动更丝滑吧【……
-                }
-                if (d < 0)
-                {
-                    scroll.LineRight();
-                    scroll.LineRight();
-                    // currentNum = horOffset / (int)items.ActualWidth + 1;
-                }
-                scroll.ScrollToTop();
-            }
+            AddHandler(MouseWheelEvent, new ScrollViewerExtension().HorizontalWheelHandler(task_list), true); // 使task_list可以横向滚动
         }
     }
 
     /// <summary>
-    /// 用于重写SelectTemplate方法的类
+    /// 用于重写SelectTemplate方法的类，使得可以根据不同的计划类型绑定不同的模板
     /// </summary>
     public class TasksDataTemplate : DataTemplateSelector
     {
         public DataTemplate ImageTemplate { get; set; }
-
         public DataTemplate TextTemplate { get; set; }
 
         /// <summary>
