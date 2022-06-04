ef78dd552db1d365904e21b3809c404e123343c6
SuperSASS <32005046+SuperSASS@users.noreply.github.com>
2022-06-04 10:09:43 +0800

    Fix - 在按上一页、主页按钮时，主菜单的导航栏不会随之切换显示。

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,14 +6,16 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.ObjectModel;
+using System.Windows.Controls;
 
-// TODO: 注意：在按按钮导航时，主菜单的导航栏不会被切换，需要fix
 namespace CourseDesign.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private ObservableCollection<MeauBar> meauBars; // 主菜单列表
-        public ObservableCollection<MeauBar> MeauBars { get { return meauBars; } set { meauBars = value; RaisePropertyChanged(); } }
+        private int selectIndex;                        // 所选择的主菜单哪一项
+        public ObservableCollection<MeauBar> MeauBars { get { return meauBars; } set { meauBars = value; } }
+        public int SelectIndex { get { return selectIndex; } set { selectIndex = value; RaisePropertyChanged(); } }
 
         // 命令部分
         public DelegateCommand<MeauBar> NavigationCommand { get; private set; } // 从UI层传递MeauBars到这个导航命令
@@ -27,25 +29,60 @@ namespace CourseDesign.ViewModels
         /// MainWindow VM 的构造函数：
         /// <para>完成菜单栏、导航命令、后退命令、返回主页命令的初始化</para>
         /// </summary>
-        /// <param name="reigionManager"> 区域管理器 </param>
-        public MainWindowViewModel(IRegionManager reigionManager)
+        /// <param name="regionManager"> 区域管理器 </param>
+        public MainWindowViewModel(IRegionManager regionManager)
         {
             MeauBars = new ObservableCollection<MeauBar>();
             CreateMeauBars();
-            this.regionManager = reigionManager;
+            this.regionManager = regionManager;
+            // 命令接口实现
             NavigationCommand = new DelegateCommand<MeauBar>(Navigate);
-            GoBackCommand = new DelegateCommand(() =>
-            {
-                if (journal != null && journal.CanGoBack)
-                    journal.GoBack();
-            });
-            GoHomeCommand = new DelegateCommand(() =>
-            {
-                reigionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate("IndexView", back =>
-                {
-                    journal = back.Context.NavigationService.Journal;
-                });
-            });
+            GoBackCommand = new DelegateCommand(GoBack);
+            GoHomeCommand = new DelegateCommand(GoIndex); // 主页定为Index页面，所以更名为GoINdex
+            // TODO: 实现默认启动IndexView，但不能像下面那样简单的实现。
+            SelectIndex = -1;
+            // regionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate("IndexView");
+        }
+
+        /// <summary>
+        /// 在进行上一页和返回主页操作后，用来更新NavagationBar（菜单栏导航卡）中所选择的导航项的index，从而更新所选择的项。否则操作后导航项不会更新
+        /// </summary>
+        private void UpdateSelectIndex()
+        {
+            int index = 0;
+            foreach (MeauBar nowMeauBar in MeauBars)
+                if (nowMeauBar.NameSpace == journal.CurrentEntry.Uri.ToString())
+                    SelectIndex = index;
+                else
+                    index++;
+            //navigationBars.SelectedIndex = navigationBars.SelectedIndex - 1;
+
+            //foreach (MeauBar item in navigationBars.Items)
+            //{
+            //    m=1 + 1;
+            //    item.
+            //}
+            //ListBoxItem t = (ListBoxItem)navigationBars.Items[navigationBars.SelectedIndex-1];
+            //t.IsSelected = true;
+        }
+
+        /// <summary>
+        /// 返回上一页的实现方法
+        /// </summary>
+        private void GoBack()
+        {
+            if (journal != null && journal.CanGoBack)
+                journal.GoBack();
+            UpdateSelectIndex();
+        }
+
+        /// <summary>
+        /// 返回Index页的实现方法
+        /// </summary>
+        private void GoIndex()
+        {
+            regionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate("IndexView", back => { journal = back.Context.NavigationService.Journal; });
+            UpdateSelectIndex();
         }
 
         /// <summary>
@@ -55,14 +92,12 @@ namespace CourseDesign.ViewModels
         private void Navigate(MeauBar obj)
         {
             if (obj != null && !string.IsNullOrWhiteSpace(obj.Title))
-                regionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate(obj.NameSpace, back =>
-                {
-                    journal = back.Context.NavigationService.Journal;
-                });
+                regionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate(obj.NameSpace, back => { journal = back.Context.NavigationService.Journal; });
         }
-        
+
         /// <summary>
         /// 创建主菜单列表
+        /// <para>注意：Add的顺序需要与主菜单列表的一致！否则在上一页、返回主页时，NavigationBar的更新会混乱</para>
         /// </summary>
         void CreateMeauBars()
         {
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -5,23 +5,24 @@ using System.Windows.Controls;
 namespace CourseDesign.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// 起始页的基本页面实现逻辑【只实现先基本的如关闭、最小化之类的页面逻辑，有关数据绑定之类的则分离到了后端ViewModel中
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            navigationBars.SelectedIndex = 3;
 
-            btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; }; // 菜单栏 - 窗口最小化按钮
-            btnMax.Click += (s, e) => // 菜单栏 - 窗口最大化按钮
+            btn_min.Click += (s, e) => { this.WindowState = WindowState.Minimized; }; // 菜单栏 - 窗口最小化按钮
+            btn_max.Click += (s, e) => // 菜单栏 - 窗口最大化按钮
             {
                 if (this.WindowState == WindowState.Normal)
                     this.WindowState = WindowState.Maximized;
                 else
                     this.WindowState = WindowState.Normal;
             };
-            btnClose.Click += (s, e) => { this.Close(); }; // 菜单栏 - 窗口关闭按钮
+            btn_close.Click += (s, e) => { this.Close(); }; // 菜单栏 - 窗口关闭按钮
 
             titleBar.MouseLeftButtonDown += (s, e) => // 菜单栏 - 窗口拖动
             {
